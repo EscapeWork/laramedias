0ce74ce9badf501639ac2c722241c3554ef2ccd7
Luis Dalmolin <luis.nh@gmail.com>
2016-10-21 10:40:12 -0200

    Fixed type

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,6 @@
 
 if (! function_exists('media')) {
     function media($model, $field, $w = null, $h = null, $fit = 'resize', $crop = 'center') {
-        return asset(config('medias.url') . '/' . $model->table . '/' . $model->{$field} . '?w='.$w.'&h='.$h.'&fit='.$fit.'&crop='.$crop)
+        return asset(config('medias.url') . '/' . $model->table . '/' . $model->{$field} . '?w='.$w.'&h='.$h.'&fit='.$fit.'&crop='.$crop);
     }
 }
