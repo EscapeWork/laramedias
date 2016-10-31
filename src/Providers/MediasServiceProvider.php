07b3469a67a0abff577ad4ad0150fb5cb851202b
Luis Dalmolin <luis.nh@gmail.com>
2016-10-31 16:45:48 -0200

    Fixed proviers

--- src/Providers/MediasServiceProvider.php
+++ src/Providers/MediasServiceProvider.php
@@ -87,7 +87,6 @@ class MediasServiceProvider extends ServiceProvider
     protected function registerProviders()
     {
         $this->app->register('EscapeWork\LaraMedias\Providers\RouteServiceProvider');
-        $this->app->register('EscapeWork\LaravelSteroids\SteroidsServiceProvider');
 
         if ($this->app['request']->is(config('manager.url') . '/*')) {
             $this->app->register('EscapeWork\LaraMedias\Providers\EventServiceProvider');
